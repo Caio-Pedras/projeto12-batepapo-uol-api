65c28d792ea930851560bfb93ba52c41626492b2
CaioPedras <caio.vp@gmail.com>
2022-06-27 13:58:05 -0300

    Bonus Put+Delete

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import cors from "cors";
 import express, { json } from "express";
-import { MongoClient } from "mongodb";
+import { MongoClient, ObjectId } from "mongodb";
 import joi from "joi";
 import dayjs from "dayjs";
+import dotenv from "dotenv";
 const app = express();
 
 app.use(cors());
 app.use(json());
-const mongoClient = new MongoClient("mongodb://localhost:27017");
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.URL);
 let db;
 mongoClient.connect().then(() => {
   db = mongoClient.db("UOL_API");
@@ -20,25 +23,18 @@ app.post("/participants", async (req, res) => {
   });
   const validation = participantSchema.validate(participant);
   if (validation.error) {
-    console.log(validation.error, participantSchema, participant);
-    console.log("consolelog maneiro erro na validaçãod o login");
-    res.sendStatus(422, "consolelog maneiro erro na validaçãod o login");
+    console.log("erro na validaçãod o login");
+    res.sendStatus(422);
     return;
   }
   try {
-    // await mongoClient.connect();
-    // const db = mongoClient.db("UOL_API");
-
     const participants = await db.collection("participants").find({}).toArray();
     const checkParticipant = participants.some(
       (item) => item.name === participant.name
     );
     if (checkParticipant) {
-      console.log(409, "consolelog maneiro erro na checagem do participante");
-      res.sendStatus(
-        409,
-        "consolelog maneiro erro na checagem do participante"
-      );
+      console.log("erro na checagem do participante");
+      res.sendStatus(409);
       return;
     }
 
@@ -55,22 +51,17 @@ app.post("/participants", async (req, res) => {
     res.sendStatus(201);
     // mongoClient.close();
   } catch (err) {
-    console.log(err, "consolelog maneiro erro no catch do post participante");
+    console.log(err, "erro no catch do post participante");
     res.status(500).send(err, "erro");
-    // mongoClient.close();
   }
 });
 app.get("/participants", async (req, res) => {
   try {
-    // await mongoClient.connect();
-    // const db = mongoClient.db("UOL_API");
     const participants = await db.collection("participants").find({}).toArray();
     res.send(participants);
-    // mongoClient.close();
   } catch (err) {
-    console.log(err, "consolelog maneiro erro no get participantes");
+    console.log(err, "erro no get participantes");
     res.status(500).send(err);
-    // mongoClient.close();
   }
 });
 
@@ -83,35 +74,26 @@ app.post("/messages", async (req, res) => {
     to: joi.string().required(),
     text: joi.string().required(),
     type: joi.string().valid("message", "private_message").required(),
-    time: joi.required(),
+    time: joi.string().required(),
   });
   const validation = messageSchema.validate(message, { abortEarly: false });
   if (validation.error) {
-    console.log("consolelog maneiro erro validação do  post message");
-    res.sendStatus(422, validation.error);
+    console.log("erro validação do  post message");
+    res.sendStatus(422);
     return;
   }
-
   try {
-    // await mongoClient.connect();
-    // const db = mongoClient.db("UOL_API");
-
     await db.collection("messages").insertOne(message);
     res.sendStatus(201);
-    // mongoClient.close();
   } catch (err) {
     res.status(500).send(err);
-    console.log("consolelog maneiro erro no envio de mensagem");
-    // mongoClient.close();
+    console.log("erro no envio de mensagem");
   }
 });
 app.get("/messages", async (req, res) => {
   const { limit } = req.query;
   const { user } = req.headers;
   try {
-    // await mongoClient.connect();
-    // const db = mongoClient.db("UOL_API");
-
     let messages = await db.collection("messages").find({}).toArray();
     let messagesDisplay = messages.filter((message) => {
       const publicMessage = message.type === "message";
@@ -119,28 +101,24 @@ app.get("/messages", async (req, res) => {
       return publicMessage || userPrivateMessage;
     });
 
-    messagesDisplay = messages.splice(0, limit);
+    messagesDisplay = messages.reverse().splice(0, limit);
+    messagesDisplay = messagesDisplay.reverse();
     res.send(messagesDisplay);
-    // mongoClient.close();
   } catch (err) {
-    console.log("consolelog maneiro erro pegando as mensagens");
+    console.log("erro pegando as mensagens");
     res.status(500).send(err);
-    // mongoClient.close();
   }
 });
 
 app.post("/status", async (req, res) => {
   const { user } = req.headers;
   try {
-    // await mongoClient.connect();
-    // const db = mongoClient.db("UOL_API");
-
     const participant = await db
       .collection("participants")
       .findOne({ name: user });
 
     if (!participant) {
-      console.log("erro na verifica~çao do participante no status");
+      console.log("erro na verificaçao do participante no status");
       res.sendStatus(404);
       return;
     }
@@ -149,11 +127,9 @@ app.post("/status", async (req, res) => {
       .collection("participants")
       .updateOne({ name: user }, { $set: { lastStatus: Date.now() } });
     res.sendStatus(200);
-    // mongoClient.close();
   } catch (err) {
     console.log("erro no status", err);
     res.status(500).send(err);
-    // mongoClient.close();
   }
 });
 const CHECKOUT_TIME = 15000;
@@ -182,8 +158,74 @@ setInterval(async () => {
     }
   } catch (err) {
     console.log("erro no checkou de usuarios", err);
-    res.sendStatus(500);
   }
 }, CHECKOUT_TIME);
+
+app.delete("/messages/:ID_DA_MENSAGEM", async (req, res) => {
+  const from = req.headers.user;
+  const messageId = req.params.ID_DA_MENSAGEM;
+
+  try {
+    const message = await db
+      .collection("messages")
+      .findOne({ _id: new ObjectId(messageId) });
+
+    if (!message) {
+      res
+        .status(404)
+        .send("não foi possiver encontrar uma mensagem com esse id");
+      return;
+    }
+    if (message.from !== from) {
+      res.status(401).send("o usuario não é dono dessa mensagem");
+      return;
+    }
+    await db.collection("messages").deleteOne({ _id: new ObjectId(messageId) });
+  } catch (err) {
+    console.log("erro ao deletar mensagem", err);
+    res.status(500).send(err);
+  }
+});
+
+app.put("/messages/:ID_DA_MENSAGEM", async (req, res) => {
+  const { to, text, type } = req.body;
+  const messageId = req.params.ID_DA_MENSAGEM;
+  try {
+    const participant = await db
+      .collection("participants")
+      .findOne({ name: req.headers.user });
+    if (!participant) {
+      res.sendStatus(422);
+      return;
+    }
+    const messageSchema = joi.object({
+      to: joi.string().required(),
+      text: joi.string().required(),
+      type: joi.string().valid("message", "private_message").required(),
+    });
+
+    const message = {
+      to,
+      text,
+      type,
+    };
+    const validation = messageSchema.validate(message, { abortEarly: false });
+    if (validation.error) {
+      console.log("erro no schema da edição de mensagem");
+      res.sendStatus(422);
+      return;
+    }
+    await db
+      .collection("messages")
+      .updateOne(
+        { _id: new ObjectId(messageId) },
+        { $set: { text: text, time: dayjs().format("HH:mm:ss") } }
+      );
+    res.sendStatus(201);
+  } catch (err) {
+    console.log("erro ao editar mensagem", err);
+    res.status(500).send(err);
+  }
+});
 
 app.listen(5000);
